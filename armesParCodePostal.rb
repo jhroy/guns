1b032f283fd494e4940ddd83ea8b7ed3df4f740e
Jean-Hugues Roy <jh@jhroy.ca>
2014-06-18 11:22:26 -0400

    Update armesParCodePostal.rb

--- armesParCodePostal.rb
+++ armesParCodePostal.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/env ruby
 
+# Jean-Hugues Roy 2014 GPL v3
+# Ce programme est un logiciel libre ;
+# vous pouvez le redistribuer ou le modifier suivant les termes de la GNU General Public License
+# telle que publiée par la Free Software Foundation;
+# soit la version 3 de la licence, soit (à votre gré) toute version ultérieure.
+# Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ;
+# sans même la garantie tacite de QUALITÉ MARCHANDE ou d'ADÉQUATION à UN BUT PARTICULIER.
+# Consultez la GNU General Public License pour plus de détails.
+# Vous devez avoir reçu une copie de la GNU General Public License en même temps que ce programme ;
+# si ce n'est pas le cas, consultez <http://www.gnu.org/licenses>
+
 require "csv"
 
 # construction d'une liste des codes postaux à 2 caractères
